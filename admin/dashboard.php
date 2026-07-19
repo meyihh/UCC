575880d41953c194127cdb08e563d9faddfef34f
Ian Kyron <chaniankyron.bsit@gmail.com>
2026-07-19 14:51:24 +0800

    tanggalin ang dash sa dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -26,7 +26,7 @@ $navbarQuery = mysqli_query(
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>UCC Admin Dashboard</title>
+    <title>UCC Admin DashDashboard</title>
 
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
